84d724abd69767d8c7ff6b8d7d87a2f7ed1bab09
Fabien Mansoubi <fabienmp@gmail.com>
2019-05-14 17:40:26 -0700

    small fixes for spotify command

--- liri.js
+++ liri.js
@@ -13,14 +13,14 @@ process.argv.forEach(function (val, index, array) {
     if (index == 2) {
         userCommand = val;
         if (validCommands.indexOf(userCommand) == -1) {
-            console.log(chalk.yellow('Oops! The command you chose is not valid. Use -help command to see available commands.'));
+            console.log(chalk.red('Oops! The command you chose is not valid. Use -help command to see available commands.'));
             process.exit();
         }
-        console.log('Command: ' + userCommand);
+        console.log('Command: ' + chalk.blue(userCommand));
     }
     if (index == 3) {
         userCommandParam = val;
-        console.log('Param: ' + userCommandParam);
+        console.log('Param: ' + chalk.blue(userCommandParam));
     }
 });
 
@@ -33,30 +33,29 @@ if (userCommand == '-help') {
 }
 
 if (userCommand == 'spotify-this-song') {
-    if (userCommandParam != null && userCommandParam != '') {
-        console.log("Searching Spotify DB for track " + userCommandParam);
-        spotify
-            .search({
-                type: 'track',
-                query: userCommandParam
-            })
-            .then(function (response) {
-                //console.log(response);
-                if (response != null && response.tracks.total > 0) {
-                    var trackObject = response.tracks.items[0];
-
-                    if (trackObject.artists.length > 0)
-                        console.log('* Artist: ' + trackObject.artists[0].name);
-                    console.log('* Song Name: ' + trackObject.name);
-                    console.log('* Preview URL: ' + trackObject.preview_url);
-                    console.log('* Album: ' + trackObject.album.name);
-
-                }
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
-    } else {
-
+    if (userCommandParam == null || userCommandParam == '') {
+        userCommandParam = 'The Sign Ace Of Base';
     }
+    console.log(chalk.red("\r -- Searching Spotify DB for track '" + userCommandParam + "' -- \r"));
+    spotify
+        .search({
+            type: 'track',
+            query: userCommandParam
+        })
+        .then(function (response) {
+            //console.log(response);
+            if (response != null && response.tracks.total > 0) {
+                var trackObject = response.tracks.items[0];
+
+                if (trackObject.artists.length > 0)
+                    console.log('* Artist: ' + chalk.yellow(trackObject.artists[0].name));
+                console.log('* Song Name: ' + chalk.yellow(trackObject.name));
+                console.log('* Preview URL: ' + chalk.yellow(trackObject.preview_url));
+                console.log('* Album: ' + chalk.yellow(trackObject.album.name));
+
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 }
